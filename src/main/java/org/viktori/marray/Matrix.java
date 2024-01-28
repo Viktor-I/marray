a9404c7c45d9c4d5ff026add252891f140d5af75
Viktor-I <viktor.ingemansson@gmail.com>
2024-01-28 17:43:19 +0100

    Extended Matrix interface

--- src/main/java/org/viktori/marray/Matrix.java
+++ src/main/java/org/viktori/marray/Matrix.java
@@ -1,10 +1,7 @@
 package org.viktori.marray;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.function.IntFunction;
 import java.util.function.Predicate;
 
 public interface Matrix<E> extends Collection<E> {
@@ -14,7 +11,7 @@ public interface Matrix<E> extends Collection<E> {
     /**
      * Get the element at a given index in this list.
      *
-     * @param rowIndex the row index of the element to be returned
+     * @param rowIndex    the row index of the element to be returned
      * @param columnIndex the column index of the element to be returned
      * @return the element at (rowIndex, columnIndex) in this matrix
      * @throws ArrayIndexOutOfBoundsException if any index &lt; 0 || index &gt;= rows()/columns()
@@ -30,10 +27,10 @@ public interface Matrix<E> extends Collection<E> {
      * {@code fromRow} and {@code toRow} are equal, or {@code fromColumn and @toColumn} are equal
      * , the returned matrix is empty.)
      *
-     * @param fromRow low row endpoint (inclusive) of the subMatrix
-     * @param toRow   high row endpoint (exclusive) of the subMatrix
+     * @param fromRow    low row endpoint (inclusive) of the subMatrix
+     * @param toRow      high row endpoint (exclusive) of the subMatrix
      * @param fromColumn low column endpoint (inclusive) of the subMatrix
-     * @param toColumn high row endpoint (exclusive) of the subMatrix
+     * @param toColumn   high row endpoint (exclusive) of the subMatrix
      * @return a view of the specified rectangle within this matrix
      * @throws ArrayIndexOutOfBoundsException for an illegal endpoint index value
      *                                        ({@code fromIndex < 0 || toIndex > rows/columns})
@@ -71,11 +68,12 @@ public interface Matrix<E> extends Collection<E> {
     Array<E> column(int columnIndex);
 
     // Transformations
+
     /**
      * Returns a new matrix containing all the elements in this matrix, but rotated based
      * based on the specified rotation.
      *
-     * @param  rotation {@link Rotation} to apply
+     * @param rotation {@link Rotation} to apply
      * @return a new matrix with the same elements, but rotated according to the specified rotation
      * @see Rotation
      */
@@ -84,10 +82,261 @@ public interface Matrix<E> extends Collection<E> {
     /**
      * Returns a new matrix containing all the elements in this matrix, but mirrored
      * based on the specified mirroring axis.
+     *
      * @param axis {@link Axis} to mirror.
      * @return a new matrix with the same elements, but mirrored on the specified axis
      */
     Matrix<E> toMirroredMatrix(Axis axis);
+
+    // Query operations
+
+    /**
+     * Returns the number of elements in this matrix.  If this matrix
+     * contains more than {@code Integer.MAX_VALUE} elements, returns
+     * {@code Integer.MAX_VALUE}.
+     *
+     * @return the number of elements in this matrix
+     */
+    int size();
+
+    /**
+     * Returns the number of rows in this matrix.
+     *
+     * @return the number of rows in this matrix
+     */
+    int rows();
+
+    /**
+     * Returns the number of columns in this matrix.
+     *
+     * @return the number of columns in this matrix
+     */
+    int columns();
+
+    /**
+     * Returns {@code true} if this matrix is a square matrix. It is a square matrix if
+     * the number of rows equals the number of columns. (If rows == columns).
+     *
+     * @return {@code true} if this matrix has the same amount of rows as columns
+     */
+    boolean isSquare();
+
+    /**
+     * Returns an array containing all of the elements in this matrix.
+     * If this matrix makes any guarantees as to what order its elements
+     * are returned by its iterator, this method must return the elements in
+     * the same order. The returned array's {@linkplain Class#getComponentType
+     * runtime component type} is {@code Object}.
+     *
+     * <p>Note that the returned array is not two-dimensional and will thus return all
+     * elements in all rows. Use {@link #toArray2D()} for a two-dimensional array.</p>
+     *
+     * <p>The returned array will be "safe" in that no references to it are
+     * maintained by this matrix.  (In other words, this method must
+     * allocate a new array even if this matrix is backed by an array).
+     * The caller is thus free to modify the returned array.
+     *
+     * @apiNote
+     * This method acts as a bridge between array-based and collection-based APIs.
+     * It returns an array whose runtime type is {@code Object[]}.
+     * Use {@link #toArray(Object[]) toArray(T[])} to reuse an existing
+     * array, or use {@link #toArray(IntFunction)} to control the runtime type
+     * of the array.
+     *
+     * @return an array, whose {@linkplain Class#getComponentType runtime component
+     * type} is {@code Object}, containing all of the elements in this matrix
+     */
+    Object[] toArray();
+
+    /**
+     * Returns an array containing all of the elements in this matrix;
+     * the runtime type of the returned array is that of the specified array.
+     * If the matrix fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this matrix.
+     *
+     * <p>If this matrix fits in the specified array with room to spare
+     * (i.e., the array has more elements than this matrix), the element
+     * in the array immediately following the end of the matrix is set to
+     * {@code null}.  (This is useful in determining the length of this
+     * collection <i>only</i> if the caller knows that this matrix does
+     * not contain any {@code null} elements.)
+     *
+     * <p>If this matrix makes any guarantees as to what order its elements
+     * are returned by its iterator, this method must return the elements in
+     * the same order.
+     *
+     * <p>Note that the returned array is not two-dimensional and will thus return all
+     * elements in all rows. Use {@link #toArray2D(T[][])} for a two-dimensional array.</p>
+     *
+     * @apiNote
+     * This method acts as a bridge between array-based and collection-based APIs.
+     * It allows an existing array to be reused under certain circumstances.
+     * Use {@link #toArray()} to create an array whose runtime type is {@code Object[]},
+     * or use {@link #toArray(IntFunction)} to control the runtime type of
+     * the array.
+     *
+     * <p>Suppose {@code x} is a matrix known to contain only strings.
+     * The following code can be used to dump the matrix into a previously
+     * allocated {@code String} array:
+     *
+     * <pre>
+     *     String[] y = new String[SIZE];
+     *     ...
+     *     y = x.toArray(y);</pre>
+     *
+     * <p>The return value is reassigned to the variable {@code y}, because a
+     * new array will be allocated and returned if the matrix {@code x} has
+     * too many elements to fit into the existing array {@code y}.
+     *
+     * <p>Note that {@code toArray(new Object[0])} is identical in function to
+     * {@code toArray()}.
+     *
+     * @param <T> the component type of the array to contain the matrix
+     * @param a the array into which the elements of this matrix are to be
+     *        stored, if it is big enough; otherwise, a new array of the same
+     *        runtime type is allocated for this purpose.
+     * @return an array containing all of the elements in this matrix
+     * @throws ArrayStoreException if the runtime type of any element in this
+     *         matrix is not assignable to the {@linkplain Class#getComponentType
+     *         runtime component type} of the specified array
+     * @throws NullPointerException if the specified array is null
+     */
+    <T> T[] toArray(T[] a);
+
+    /**
+     * Returns an array containing all of the elements in this collection,
+     * using the provided {@code generator} function to allocate the returned array.
+     *
+     * <p>If this collection makes any guarantees as to what order its elements
+     * are returned by its iterator, this method must return the elements in
+     * the same order.
+     *
+     * <p>Note that the returned array is not two-dimensional and will thus return all
+     * elements in all rows. Use {@link #toArray2D(IntFunction<T[][]>)} for a two-dimensional array.</p>
+     *
+     * @apiNote
+     * This method acts as a bridge between array-based and collection-based APIs.
+     * It allows creation of an array of a particular runtime type. Use
+     * {@link #toArray()} to create an array whose runtime type is {@code Object[]},
+     * or use {@link #toArray(Object[]) toArray(T[])} to reuse an existing array.
+     *
+     * <p>Suppose {@code x} is a matrix known to contain only strings.
+     * The following code can be used to dump the matrix into a newly
+     * allocated array of {@code String}:
+     *
+     * <pre>
+     *     String[] y = x.toArray(String[]::new);</pre>
+     *
+     * @implSpec
+     * The default implementation calls the generator function with zero
+     * and then passes the resulting array to {@link #toArray(Object[]) toArray(T[])}.
+     *
+     * @param <T> the component type of the array to contain the collection
+     * @param generator a function which produces a new array of the desired
+     *                  type and the provided length
+     * @return an array containing all of the elements in this collection
+     * @throws ArrayStoreException if the runtime type of any element in this
+     *         matrix is not assignable to the {@linkplain Class#getComponentType
+     *         runtime component type} of the generated array
+     * @throws NullPointerException if the generator function is null
+     */
+    default <T> T[] toArray(IntFunction<T[]> generator) {
+        return toArray(generator.apply(0));
+    }
+
+    /**
+     * Returns a two-dimensional array containing all of the elements in this matrix.
+     * The outer array contains the rows and the inner array each column for every row.
+     *
+     * <p>The returned array will be "safe" in that no references to it are
+     * maintained by this matrix.  (In other words, this method must
+     * allocate a new array even if this matrix is backed by an array).
+     * The caller is thus free to modify the returned two-dimensional array.
+     *
+     * @return a row-based array with column-arrays inside, whose {@linkplain Class#getComponentType runtime component
+     * type} is {@code Object}, containing all of the elements in this matrix
+     */
+    Object[][] toArray2D();
+
+    /**
+     * Returns a two-dimensional array containing all of the elements in this matrix.
+     * The outer array contains the rows and the inner array each column for every row.
+     *
+     * <p>If this matrix fits in the specified array with room to spare
+     * (i.e., the array has more elements than this matrix), the element
+     * in the array immediately following the end of the matrix is set to
+     * {@code null}.  (This is useful in determining the length of this
+     * collection <i>only</i> if the caller knows that this matrix does
+     * not contain any {@code null} elements.)
+     *
+     * <p>If this matrix makes any guarantees as to what order its elements
+     * are returned by its iterator, this method must return the elements in
+     * the same order.
+     *
+     * <p>Suppose {@code x} is a matrix known to contain only strings.
+     * The following code can be used to dump the matrix into a previously
+     * allocated {@code String} two-dimensional-array:
+     *
+     * <pre>
+     *     String[][] y = new String[ROWS][COLUMNS];
+     *     ...
+     *     y = x.toArray2D(y);</pre>
+     *
+     * <p>The return value is reassigned to the variable {@code y}, because a
+     * new two-dimensional array will be allocated and returned if the matrix {@code x} has
+     * too many elements to fit into the existing two-dimensional array {@code y}.
+     *
+     * <p>Note that {@code toArray2D(new Object[0][0])} is identical in function to
+     * {@code toArray2D()}.
+     *
+     * @param <T> the component type of the two-dimensional array to contain the matrix
+     * @param a the two-dimensional array into which the elements of this matrix are to be
+     *        stored, if it is big enough; otherwise, a new two-dimensional array of the same
+     *        runtime type is allocated for this purpose.
+     * @return a row-based array with column-arrays inside containing all of the elements in this matrix
+     * @throws ArrayStoreException if the runtime type of any element in this
+     *         matrix is not assignable to the {@linkplain Class#getComponentType
+     *         runtime component type} of the specified array
+     * @throws NullPointerException if the specified array is null
+     */
+    <T> T[][] toArray2D(T[][] a);
+
+    /**
+     * Returns a two-dimensional array containing all of the elements in this collection,
+     * using the provided {@code generator} function to allocate the returned two-dimensional array.
+     * The outer array contains the rows and the inner array each column for every row.
+     *
+     * <p>If this collection makes any guarantees as to what order its elements
+     * are returned by its iterator, this method must return the elements in
+     * the same order.
+     *
+     * <p>Note that the returned array is not two-dimensional and will thus return all
+     * elements in all rows. Use {@link #toArray2D(IntFunction<T[][]>)} for a two-dimensional array.</p>
+     *
+     * <p>Suppose {@code x} is a matrix known to contain only strings.
+     * The following code can be used to dump the matrix into a newly
+     * allocated two-dimensional array of {@code String}:
+     *
+     * <pre>
+     *     String[][] y = x.toArray2D(String[][]::new);</pre>
+     *
+     * @implSpec
+     * The default implementation calls the generator function with zero
+     * and then passes the resulting array to {@link #toArray2D(Object[][]) toArray2D(T[][])}.
+     *
+     * @param <T> the component type of the array to contain the matrix
+     * @param generator a function which produces a new array of the desired
+     *                  type and the provided length
+     * @return an array containing all of the elements in this matrix
+     * @throws ArrayStoreException if the runtime type of any element in this
+     *         matrix is not assignable to the {@linkplain Class#getComponentType
+     *         runtime component type} of the generated array
+     * @throws NullPointerException if the generator function is null
+     */
+    default <T> T[][] toArray2D(IntFunction<T[][]> generator) {
+        return toArray2D(generator.apply(0));
+    }
 
     // Comparison and hashing
 
@@ -339,4 +588,5 @@ public interface Matrix<E> extends Collection<E> {
          */
         BOTH,
     }
+
 }
---
+++ src/main/java/org/viktori/marray/function/MatrixIndexFunction.java
@@ -0,0 +1,24 @@
+package org.viktori.marray.function;
+
+import java.util.function.Function;
+
+/**
+ * Represents a function that accepts row and column indices and produces a
+ * result.
+ *
+ * <p>This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #valueOf(int, int)}.
+ *
+ * @param <E> the type of the result of the function
+ */
+@FunctionalInterface
+public interface MatrixIndexFunction<E> {
+    /**
+     * Applies this function to the given argument indices.
+     *
+     * @param rowIndex    the row index to apply the function to
+     * @param columnIndex the column index to apply the function to
+     * @return the function result
+     */
+    E valueOf(int rowIndex, int columnIndex);
+}
