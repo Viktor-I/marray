dad7ac87b0c9e88330b30d2f312defaaf48f0e2d
Viktor-I <viktor.ingemansson@gmail.com>
2024-02-11 17:48:28 +0100

    fail test

--- src/test/java/org/viktori/matteray/ImmutableArrayTest.java
+++ src/test/java/org/viktori/matteray/ImmutableArrayTest.java
@@ -32,7 +32,7 @@ public class ImmutableArrayTest {
     @Test
     public void testVarargsConstructorWhenEmpty() {
         Array<String> array = new ImmutableArray<>();
-        assertEquals(0, array.size());
+        assertEquals(2, array.size());
         assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> array.get(0));
     }
 
